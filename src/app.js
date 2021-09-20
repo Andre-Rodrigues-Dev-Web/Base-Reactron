c5469c78fe99bea167f1ee2873b53bfd4f29d1b6
Andre-Rodrigues-Dev-Web <andrelaurentinomg@gmail.com>
2021-09-20 14:21:11 -0300

    update arquivos

--- src/app.js
+++ src/app.js
@@ -1,38 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Dialog from '@material-ui/core/Dialog';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import DialogContent from '@material-ui/core/DialogContent';
-import AppBar from './components/app-bar';
-import Welcome from './components/welcome';
+import React from 'react'
+//Const
+import {
+    Template
+} from './components/template/style'
 
-export default class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            pageRouted: Welcome
-        };
-    }
+export default function app() {
+    return (
+        <Template>
 
-    routePage = (page) => {
-        this.setState({ pageRouted: page })
-    }
-
-    render() {
-        return (
-            <div>
-                <Dialog fullScreen={Boolean("true")} open={Boolean("true")}>
-                    <DialogTitle style={{ padding: 0 }}>
-                        <AppBar mainApp={this}></AppBar>
-                    </DialogTitle>
-
-                    <DialogContent className={'mainContent'}>
-                        <Router>
-                            <Route component={this.state.pageRouted} />
-                        </Router>
-                    </DialogContent>
-                </Dialog>
-            </div>
-        );
-    }
+        </Template>
+    )
 }
---
+++ src/components/template/style.js
@@ -0,0 +1,11 @@
+import style from "styled-components";
+
+const Template = style.div`
+    background-color: #eee;
+    min-height: 100vh;
+    width: 100vw;
+`;
+
+export{
+    Template
+}
